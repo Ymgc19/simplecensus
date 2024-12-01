fc13232a7f9603d78e051523813639fd6bd939b5
Ymgc19 <username@example.com>
2024-12-01 15:40:45 +0900

    2020年のデータ取得追加

---
+++ R/smc.get_census_2015.R
@@ -0,0 +1,18 @@
+#' @title to read files
+#' @description \code{smc.get_census_2015}
+#' @export
+
+smc.get_census_2015 <- function(pref_code){
+  library(simplecensus)
+  # データ取得
+  shp <- simplecensus::smc.read_census_shp(pref_code)
+  census <- simplecensus::smc.read_census_2015(pref_code)
+  # 結合
+  df <- left_join(shp, census)
+  # 変数整形
+  output <- df %>% 
+    simplecensus::smc.create_variables_2015() %>% 
+    simplecensus::smc.convert_percentage_vars_2015()
+  # 出力
+  return(output)
+}
