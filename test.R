1f043b3ef68941d783975841987576868ca0e2cc
Ymgc19 <username@example.com>
2026-04-04 13:18:33 +0900

    dirの設定方法

--- test.R
+++ test.R
@@ -16,4 +16,26 @@ smc.collect_census_mesh_2015(1)
 
 
 
-ho
+
+
+
+
+m <- simplecensus::smc.read_census_2020(4)
+m <- m %>% mutate(KEY_CODE = as.numeric(KEY_CODE))
+s <- simplecensus::smc.read_census_shp(4)
+s <- s %>% mutate(KEY_CODE = as.numeric(KEY_CODE))
+
+ms <- left_join(s, m)
+ms <- ms %>% 
+  simplecensus::smc.create_variables_2020() %>% 
+  simplecensus::smc.convert_percentage_vars_2020()
+
+ms %>% ggplot() +
+  geom_sf(
+    aes(fill = smc.per.population0_4), color = NA
+  ) +
+  scale_fill_gradient(
+    low = "turquoise", high = "tomato"
+  )
+
+#ms$smc.per.unemployed %>% hist
